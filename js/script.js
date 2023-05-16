a8dfb82e8f06933a9abf469acf5684e1a459396a
Nekoma59 <renaud.persinet@gmail.com>
2023-05-16 13:09:21 +0200

    Test javascript 2

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function switchMode() {
     let moon = document.getElementById ("moon");
       if(moon.className=="moon"){
         moon.className="sun";
-        document.container .right_Side.style.backgroundColor = "#141D26";
-        document..container .right_Side.style.color = "#fff";
+        document.body.style.backgroundColor = "#141D26";
+        document.body.style.color = "#fff";
       }
     else {
       moon.className ="moon";
-      document..container .right_Side.style.backgroundColor = "#fff";
-      document..container .right_Side.style.color = "#000";
+      document.body.style.backgroundColor = "#fff";
+      document.body.style.color = "#000";
     }
   }
   
