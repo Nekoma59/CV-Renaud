50baf7c0555e84738183d5a5bfff6efe4cbc96eb
Nekoma59 <renaud.persinet@gmail.com>
2023-05-16 15:07:13 +0200

    Test JS nightmode

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function switchMode() {
+function switchMode() 
+{
     let moon = document.getElementById ("moon");
       if(moon.className=="moon"){
         moon.className="sun";
-        container.right_Side.style.backgroundColor = "#141D26";
-        container.right_Side.style.color = "#fff";
+        document.container .right_Side.style.backgroundColor = "#141D26";
+        document.container .right_Side.style.color = "#fff";
       }
     else {
       moon.className ="moon";
-      container.right_Side.style.backgroundColor = "#fff";
-      container.right_Side.style.color = "#000";
+      document.container .right_Side.style.backgroundColor = "#fff";
+      document.container .right_Side.style.color = "#141D26";
     }
   }
   
