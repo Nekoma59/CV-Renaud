92b1ea557a42fd98d402b4d57aa577654218c35d
Nekoma59 <renaud.persinet@gmail.com>
2023-05-19 10:34:56 +0200

    Lien hypertext

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function switchMode() {
-    let moon = document.getElementById ("moon");
-      if(moon.className=="moon"){
-        moon.className="sun";
-        document.body.style.backgroundColor = "#lightblue";
-        document.body.style.color = "#fff";
-      }
-    else {
-      moon.className ="moon";
-      document.body.style.backgroundColor = "#lightblue"
-      document.body.style.color = "#000";
+  let moon = document.getElementById ("moon");
+    if(moon.className=="moon"){
+      moon.className="sun";
+      document.body.backgroundColor = "#000";
+      document.body.style.color = "#fff";
     }
+  else {
+    moon.className ="moon";
+    document.body.backgroundColor = "#fff";
+    document.body.style.color = "#000";
   }
-  
+}
