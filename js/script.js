eda219ae5e50e5306caa7b202344605ca4cac728
Nekoma59 <renaud.persinet@gmail.com>
2023-05-19 13:53:26 +0200

    probleme JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-function switchMode() {
-  let moon = document.getElementById ("moon");
-    if(moon.className=="moon"){
-      moon.className="sun";
-      document.getElementsByClassName("right_Side")[0].backgroundColor = "#000";
-      document.getElementsByClassName("right_Side")[0].style.color = "#fff";
-    }
-  else {
-    moon.className ="moon";
-    document.getElementsByClassName("right_Side"[0]).backgroundColor = "#fff";
-    document.getElementsByClassName("right_Side")[0].style.color = "#000";
-  }
-}
+const darkMode = document.getElementById('dark-mode');
+const body = document.querySelector('.body');
+
+darkMode.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+    body.classList.toggle('dark');
+});
